573e6fb4e5bd0741940c9be8cd909e916459cfbd
debreo <danellebreo@gmail.com>
2015-10-27 23:06:32 +0000

    second commit

--- Parser2.py
+++ Parser2.py
@@ -11,10 +11,14 @@ def main():
     print "*"*10
     #for row in tables[2].rows:
     #    print row.cells[0].paragraphs[0].text + row.cells[1].paragraphs[0].text
+    calendar = []
     for table in wordDoc:
         for row in table.rows:
             for cell in row.cells:
-                print cell.text
+                if "\n" in (cell.text).encode('utf-8'):
+                    print cell.text.encode('utf-8')
+                #print cell.text
+                
 
 
 
