3cfcdacf3d5aace16b069bc6c01cec13a621d052
debreo <danellebreo@gmail.com>
2015-10-27 23:10:18 +0000

    Removed prints

--- Parser2.py
+++ Parser2.py
@@ -6,18 +6,12 @@ def main():
     file = "Syllabus.docx"
     document = Document(file)
     wordDoc = document.tables
-    print "Extracting Text from Syllabus"
-    print ""
-    print "*"*10
-    #for row in tables[2].rows:
-    #    print row.cells[0].paragraphs[0].text + row.cells[1].paragraphs[0].text
     calendar = []
     for table in wordDoc:
         for row in table.rows:
             for cell in row.cells:
                 if "\n" in (cell.text).encode('utf-8'):
                     print cell.text.encode('utf-8')
-                #print cell.text
                 
 
 
