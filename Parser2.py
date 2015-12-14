3af233ba6b4f9de3a4650364dc837df6c130a6b6
debreo <danellebreo@gmail.com>
2015-12-14 18:15:14 +0000

    Improved Parser

--- Parser2.py
+++ Parser2.py
@@ -5,27 +5,73 @@ import json
 import yaml
 from collections import OrderedDict
 
+def syllaparsed(parsed):
+    #pass
+    #dates = []
+    #assignments = []
+    #print parsed
+    #if re.findall('\n?\w+\s\d+\/\d+\n?',parsed):
+        #print parsed
+        #dates.append(parsed.strip())
+    #else:
+        #assignments.append(parsed.replace("\n"," "))
+    
+    print parsed.strip().replace("\n"," ")
+    #print dates,assignments
+    #print assignments
+    #duedates = OrderedDict(zip(dates,assignments))
+    #print duedates
+    #for h1,h2 in duedates.items():
+    #    duetext = h1,h2
+    #    print h1.encode('utf-8')+" "+ h2.encode('utf-8')
 def main():
-    file = "Syllabus.docx"
-    document = Document(file)
+    syllabus = "dbsyllabus.docx"
+    document = Document(syllabus)
     wordDoc = document.tables
-    dates = []
-    assignments = []
+    #dates = []
+    #assignments = []
     count = 0
     for table in wordDoc:
         for row in table.rows:
-            for cell in row.cells:
-                if "\n" in (cell.text).encode('utf-8') or "/" in (cell.text).encode('utf-8'):
-                    #print cell.text.encode('utf-8')
-                    if re.findall('\n?\w+\s\d+\/\d+\n?',cell.text.encode('utf-8')):
-                        
-                        dates.append((cell.text.strip()))
-                    else:
-                        assignments.append(cell.text.replace("\n"," "))
+                for cell in row.cells:
+                     if len(row.cells) == 3:
+                         if "\n" in (cell.text).encode('utf-8') or "/" in (cell.text).encode('utf-8'):
+                             if re.findall('\n?\w+\s\d+\/\d+\n?',cell.text.encode('utf-8')):
+                                 dates.append((cell.text.strip().encode('utf-8')))
+                             else:
+                                 assignments.append(((cell.text).encode('utf-8')).replace("\n"," "))
+                     elif len(row.cells) == 4:
+                         if "/" in (cell.text).encode('utf-8'):
+                             m = re.search('\d+\/\d+',cell.text.encode('utf-8'))
+                             dates.append(m.group(0))
+                             s = re.split('\d+\/\d+',cell.text.encode('utf-8'))
+                             for items in s:
+                                 if "" != items:
+                                     (assignments.append(items.strip().replace("\n"," ").replace("\xe2\x80\x93","")))
+ 
     duedates = OrderedDict(zip(dates, assignments))
-    for x,y in  duedates.items():
-        duetext = x,y
-        print x.encode('utf-8')+" "+ y.encode('utf-8') 
+    print (duedates)
+    #for table in wordDoc:
+    #    for row in table.rows:
+    #        for cell in row.cells:
+    #            if "\n" in (cell.text).encode('utf-8') or "/" in (cell.text).encode('utf-8'):
+    #                    syllaparsed(cell.text.encode('utf-8'))
+                        #if re.findall('\n?\w+\s\d+\/\d+\n?',cell.text.encode('utf-8')):
+                            #print cell.text.encode('utf-8')
+                        
+                            #dates.append((cell.text.strip()))
+                        #else:
+                            #assignments.append(cell.text.replace("\n"," "))
+    #print dates
+    #print assignments
+    #for assign in assignments:
+        #print assign
+    #duedates = OrderedDict(zip(dates, assignments))
+    #print duedates
+    
+    #for x,y in  duedates.items():
+    #    duetext = x,y
+        #print x.encode('utf-8')+" "+ y.encode('utf-8') 
 if __name__ == "__main__":
     main()
 
--- gcal.py
+++ gcal.py
@@ -14,16 +14,21 @@ import re
 import json
 import yaml
 from collections import OrderedDict
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
+import argparse
+import datetime
+#try:
+#    import argparse
+#    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+#except ImportError:
+#    flags = None
 
 SCOPES = 'https://www.googleapis.com/auth/calendar'
 CLIENT_SECRET_FILE = 'client_secret.json'
 APPLICATION_NAME = 'Syllaplan'
-
+now = datetime.datetime.now()
+#credentials = get_credentials()
+#http = credentials.authorize(httplib2.Http())
+#service = discovery.build('calendar', 'v3', http=http)
 
 def get_credentials():
     """Gets valid user credentials from storage.
@@ -53,6 +58,24 @@ def get_credentials():
         print('Storing credentials to ' + credential_path)
     return credentials
 
+#def syllaparsed(parsed):
+    #credentials = get_credentials()
+    #http = credentials.authorize(httplib2.Http())
+    #service = discovery.build('calendar', 'v3', http=http)   
+    #dates = []
+    #assignments = []
+    #dates_hw = []
+    #parsed = parsed.strip().replace("\n"," ")
+    #if re.findall('\n?\w+\s\d+\/\d+\n?',parsed):
+       # print (parsed.strip().replace("\n", " "))
+    #   dates.append(parsed)
+    #else:
+    #    assignments.append(parsed)
+    #print (parsed)
+    #created_event = service.events().quickAdd(calendarId='primary',
+    #text=parsed.strip().replace("\n"," ")).execute()
+    #print (created_event)
+
 def main():
     """Shows basic usage of the Google Calendar API.
 
@@ -62,28 +85,50 @@ def main():
     credentials = get_credentials()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('calendar', 'v3', http=http)
-    file = "Syllabus.docx"
-    document = Document(file)
-    wordDoc = document.tables
-    dates = []
-    assignments = []
-    count = 0
-    for table in wordDoc:
-        for row in table.rows:
-            for cell in row.cells:
-                if "\n" in (cell.text).encode('utf-8') or "/" in (cell.text).encode('utf-8'):
-                    if re.findall('\n?\w+\s\d+\/\d+\n?',cell.text.encode('utf-8')):
+    syllabus = argparse.ArgumentParser(prog="Syllaplan", description='Don\'t Cram Syllaplan!')
+    syllabus.add_argument("-f",
+                        dest="file",
+                        type = str,
+                        action="store",
+                        required = True,
+                        nargs = "+",
+                        help=" The File to extract")
+    args = syllabus.parse_args()  
+    for sylla in args.file:
+        document = Document(sylla)
+        wordDoc = document.tables 
+        dates = []
+        assignments = []
+        for table in wordDoc:
+            for row in table.rows:
+                for cell in row.cells:
+                    if len(row.cells) == 3:
+                        if "\n" in (cell.text).encode('utf-8') or "/" in (cell.text).encode('utf-8'):
+                            if re.findall('\n?\w+\s\d+\/\d+\n?',cell.text.encode('utf-8')):
+                                dates.append((cell.text.strip().encode('utf-8')))
+                            else:
+                                assignments.append(((cell.text).encode('utf-8')).replace("\n"," "))
+                    elif len(row.cells) == 4:
+                        if "/" in (cell.text).encode('utf-8'):
+                            m = re.search('\d+\/\d+',cell.text.encode('utf-8'))
+                            dates.append(m.group(0))
+                            s = re.split('\d+\/\d+',cell.text.encode('utf-8'))
+                            for items in s:
+                                if "" != items:
+                                    (assignments.append(items.strip().replace("\n"," ").replace("\xe2\x80\x93","")))
+                    
+        duedates = OrderedDict(zip(dates, assignments))
+        #print (duedates)
 
-                        dates.append((cell.text.strip()))
-                    else:
-                        assignments.append(cell.text.replace("\n"," "))
-    duedates = OrderedDict(zip(dates, assignments))
-    for x,y in  duedates.items():
-        duetext = x.encode('utf-8')+" "+ y.encode('utf-8')  
-        created_event = service.events().quickAdd(calendarId='primary',
-        text=duetext).execute()
-    print (created_event)
 
+        for x,y in  duedates.items():
+            #duetext = x.encode('utf-8')+" "+ y.encode('utf-8')  
+            duetext = x + "/"+str(now.year)+ " "+y
+            #print (duetext)
+            created_event = service.events().quickAdd(calendarId='primary',
+            text=duetext).execute()
+        print (created_event)
+        print ("Events have been created please check your calendar")
 
 if __name__ == '__main__':
     main()
